3f0abad30e14d373c7da5fbb62435d94d7301b95
Nithis Nandhan <e.nithisnandhan@gmail.com>
2025-06-20 06:56:24 +0000

    completed a small model using jinjas

--- models/marts/core/int_orders__pivoted.sql
+++ models/marts/core/int_orders__pivoted.sql
@@ -1,1 +1,18 @@
-select * from {{ ref('stg_stripe__payment') }}
+with payments as (
+    select * from {{ ref('stg_stripe__payment') }}
+),
+practice as (
+    select
+        order_id,
+        {%- set paymentmethods= ['credit_card','coupon','bank_transfer','gift_card'] -%}
+        {%-for i in paymentmethods-%}
+            sum(case when payment_method='{{i}}' then amount else 0 end) as {{i}}_amount
+            {%-if not loop.last-%}
+                ,
+            {%-endif%}
+        {%endfor-%}
+    from payments
+    where status='success'
+    group by order_id
+)
+select * from practice
